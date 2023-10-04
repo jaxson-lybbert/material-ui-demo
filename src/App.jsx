44b0c7635c664b1829e41a3949843c57e6ca91ef
jaxson-lybbert <jax.a.lybb@gmail.com>
2023-10-04 11:15:37 -0600

    Added card components

--- src/App.jsx
+++ src/App.jsx
@@ -63,6 +63,35 @@ const App = () => {
             </div>
           </Container>
         </div>
+        <Container maxWidth="md">
+          <Grid container spacing={4}>
+            <Grid item>
+              <Card>
+                <CardMedia
+                  image="https://source.unsplash.com/random"
+                  title="Image Title"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5">
+                    Heading
+                  </Typography>
+                  <Typography>
+                    This is media card. Use this section to describe the
+                    content.
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    View
+                  </Button>
+                  <Button size="small" color="primary">
+                    Edit
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          </Grid>
+        </Container>
       </main>
     </>
   );

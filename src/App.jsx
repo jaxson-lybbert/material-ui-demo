4efbffba10cc34e6abac918475aae0402a5834eb
jaxson-lybbert <jax.a.lybb@gmail.com>
2023-10-04 11:29:00 -0600

    Added several more component and inline styling

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,15 @@ import {
 
 import { PhotoCamera } from "@mui/icons-material";
 
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 const App = () => {
   return (
     <>
       <CssBaseline />
       <AppBar position="relative">
         <Toolbar>
-          <PhotoCamera />
+          <PhotoCamera style={{ marginRight: "20px" }} />
           <Typography variant="h6">Photo Album</Typography>
         </Toolbar>
       </AppBar>
@@ -63,36 +65,53 @@ const App = () => {
             </div>
           </Container>
         </div>
-        <Container maxWidth="md">
+        <Container style={{ padding: "40px" }} maxWidth="md">
           <Grid container spacing={4}>
-            <Grid item>
-              <Card>
-                <CardMedia
-                  image="https://source.unsplash.com/random"
-                  title="Image Title"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5">
-                    Heading
-                  </Typography>
-                  <Typography>
-                    This is media card. Use this section to describe the
-                    content.
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small" color="primary">
-                    View
-                  </Button>
-                  <Button size="small" color="primary">
-                    Edit
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card
+                  style={
+                    ({ height: "100%" },
+                    { display: "flex" },
+                    { flexDirection: "column" })
+                  }
+                >
+                  <CardMedia
+                    style={{ paddingTop: "56.25%" }}
+                    image="https://source.unsplash.com/random"
+                    title="Image Title"
+                  />
+                  <CardContent style={{ flexGrow: "1" }}>
+                    <Typography gutterBottom variant="h5">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is media card. Use this section to describe the
+                      content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                    <Button size="small" color="primary">
+                      Edit
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
           </Grid>
         </Container>
       </main>
+      <footer style={{ padding: "50px 0" }}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary">
+          Something here to give footer a purpose
+        </Typography>
+      </footer>
     </>
   );
 };
